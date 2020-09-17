9e52d22a43f7813088e2b7eef2d22db044ced630
Thomasescoffier <a.thomasescoffier@gmail.com>
2020-09-17 19:26:40 +0200

    Anim cannon, fix sky and destroyer, build done

--- Assets/Worlds/Common/Scripts/Cannon/Shooting.cs
+++ Assets/Worlds/Common/Scripts/Cannon/Shooting.cs
@@ -6,6 +6,7 @@ public class Shooting : MonoBehaviour
 {
 
     public Transform firePoint;
+    public Animator cannonAnimator;
     public GameObject bulletPrefab;
     public bool coinCannon = false;
 
@@ -13,11 +14,14 @@ public class Shooting : MonoBehaviour
     public float fireRate = 3f;
     private float timer;
     private GameObject bullet;
-
+    private bool fired;
 
     void Start()
     {
         timer = fireRate;
+
+        cannonAnimator = gameObject.GetComponent<Animator>();
+
     }
 
 
@@ -25,15 +29,21 @@ public class Shooting : MonoBehaviour
     void Update()
     {
 
+        if(!fired)
+        {
+            cannonAnimator.SetBool("Charging", true);
+        }
+
         timer -= 1 * Time.deltaTime;
 
-        if(coinCannon)
+        if (coinCannon)
         {
             if (bullet == null)
             {
 
                 if (timer <= 0)
                 {
+                    fired = true;
                     Shoot();
                 }
 
@@ -42,7 +52,8 @@ public class Shooting : MonoBehaviour
         {
             if (timer <= 0)
             {
-                 Shoot();
+                fired = true;
+                Shoot();
             }
         }
 
@@ -51,11 +62,14 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
+        cannonAnimator.SetBool("Charging", false);
+
         bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
 
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
 
+        fired = false;
         timer = fireRate;
     }
 
